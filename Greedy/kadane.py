53db98646291b4de92df1185d84ed8733ee8df63
Aditya Mishra <adi.adityamishra007@gmail.com>
2023-07-05 23:40:46 +0530

    Kadane variation

--- Greedy/kadane.py
+++ Greedy/kadane.py
@@ -14,3 +14,41 @@ def kadane(arr):
     return (maxSofar,start,end)
 
 print(kadane([-2,1,-3,4,-1,2,1,-5,4]))
+
+
+"""
+Given a binary array nums, you should delete one element from it.
+Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
+
+"""
+
+def longestSubarray(nums):
+    if getIndex(nums,0) == -1:
+        return len(nums)-1
+    
+    zerocount = ans = res = zeroidx =  0
+    for i in range(len(nums)):
+        if nums[i] == 0 and zerocount == 0:
+            zerocount = 1
+            zeroidx = i
+            res += nums[i]
+            ans = max(res,ans)
+        elif nums[i] == 0 and zerocount == 1:
+            res = i - zeroidx - 1
+            zeroidx = i
+            zerocount = 1
+            ans = max(res,ans)
+        else:
+            res += nums[i]
+            ans = max(res,ans)
+    return ans
+
+def getIndex(nums,v):
+    try:
+        index_value = nums.index(v)
+        return index_value
+    except ValueError:
+        index_value = -1
+        return index_value
+
+print(longestSubarray([0,1,1,1,0,1,1,0,1]))
